4794581dcda45ca984e23560d128f527a32a7a1d
gosia-b <malgorzata.maria.bak@gmail.com>
2022-08-17 23:36:49 +0200

    Working GET request

--- main.py
+++ main.py
@@ -1,48 +1,18 @@
+import pandas as pd
 from flask import Flask
-from flask_restful import Resource, Api
+
+from database import connect_to_db
 
 app = Flask(__name__)
-api = Api(app)
 
 
-users = {
-    '0': {
-        'name': 'Colleen',
-        'surname': 'Brown',
-        'email': 'colleen.brown@example.com',
-        'address': '3904 First Street',
-        'phone': '(908) 256-2784'
-    },
-    '1': {
-        'name': 'Christian',
-        'surname': 'Fletcher',
-        'email': 'christian.fletcher@example.com',
-        'address': '7494 James St',
-        'phone': '(563) 518-9948'
-    },
-    '2': {
-        'name': 'Jon',
-        'surname': 'Elliott',
-        'email': 'jon.elliott@example.com',
-        'address': '8067 Stevens Creek Blvd',
-        'phone': '(551) 760-2152'
-    },
-    '3': {
-        'name': 'Joy',
-        'surname': 'Morris',
-        'email': 'joy.morris@example.com',
-        'address': '9018 W Sherman Dr',
-        'phone': '(701) 539-7575'
-    },
-}
+@app.route('/users', methods=['GET'])
+def get_users():
+    conn = connect_to_db()
+    query = 'SELECT * FROM users'
+    df = pd.read_sql(query, conn)
+    return df.to_json(orient='records')
 
-
-class User(Resource):
-    def get(self, user_id):
-        return users[user_id]
-
-
-api.add_resource(User, '/user/<user_id>')
 
 if __name__ == '__main__':
     app.run()
